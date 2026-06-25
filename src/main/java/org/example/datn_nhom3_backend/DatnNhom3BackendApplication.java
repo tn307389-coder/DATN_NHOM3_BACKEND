d1b619bacc2ff4fac1e951157a8a1d825f80a648
tn307389-coder <tn307389@gmail.com>
2026-06-25 16:03:31 +0700

    Khoi tao backend Spring Boot

---
+++ src/main/java/org/example/datn_nhom3_backend/DatnNhom3BackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.datn_nhom3_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatnNhom3BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatnNhom3BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/datn_nhom3_backend/DatnNhom3BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.datn_nhom3_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatnNhom3BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
